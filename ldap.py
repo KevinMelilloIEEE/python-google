cce7f4970328a9e075717985b0c811182784779f
KevinMelilloIEEE <31476865+KevinMelilloIEEE@users.noreply.github.com>
2022-12-02 09:33:48 -0500

    Create ldap.py

---
+++ ldap.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os.path
+from ldap3 import Server, Connection, SAFE_SYNC, ObjectDef, AttrDef, Reader, Writer, ALL
+
+# Global Variables
+basedn = ''
+
+# Creates a connection with LDAP, reading the connection info from the credentials file
+# include needed: from ldap import connectLDAP
+# conn = connectLDAP()
+def connectLDAP():
+    global basedn
+    ldapInfo = os.path.expanduser('~/python-google-data/ldap-creds')
+    with open(ldapInfo) as f:
+      for line in f:
+        head, sep, tail = line.partition(':')
+        if head == 'uid':
+          uid = 'uid=' + tail.strip()
+        if head == 'password':
+          pWord = tail.strip()
+        if head == 'server':
+          ldapServer = tail.strip()
+        if head == 'basedn':
+          basedn = tail.strip()
+    conn = Connection(ldapServer, uid, pWord, client_strategy=SAFE_SYNC, auto_bind=True)
+    return conn
+
+# Searches LDAP for specified fields using mailalias
+# include needed: from ldap import ldapSearch
+# ldapRecord = ldapSearch(mailalias)
+def ldapSearch(mailalias):
+  conn = connectLDAP()
+  ldapRecord = conn.search(basedn, '(mailalias=' + mailalias + ')', attributes=['sn', 'givenName', 'manager', 'gaid', 'telephoneNumber'])
+  return ldapRecord
+
+# Searches LDAP for specified fields using dn
+# include needed: from ldap import ldapDN
+# ldapRecord = ldapDN(dn)
+def ldapDN(dn):
+  conn = connectLDAP()
+  ldapRecord = conn.search(basedn, '(distinguishedname' + dn + ')', attributes=['sn', 'givenName', 'manager', 'gaid', 'telephoneNumber'])
+  return ldapRecord
