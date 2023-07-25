106877d05c76f9473fe9aded2e81c51e3534e41d
KevinMelilloIEEE <31476865+KevinMelilloIEEE@users.noreply.github.com>
2023-07-25 11:04:03 -0400

    Update ldap.py

--- ldap.py
+++ ldap.py
@@ -4,12 +4,14 @@ from ldap3 import Server, Connection, SAFE_SYNC, ObjectDef, AttrDef, Reader, Wri
 
 # Global Variables
 basedn = ''
+staffbasedn = ''
+memberbasedn = ''
 
 # Creates a connection with LDAP, reading the connection info from the credentials file
 # include needed: from ldap import connectLDAP
 # conn = connectLDAP()
 def connectLDAP():
-    global basedn
+    global basedn,staffbasedn,memberbasedn
     ldapInfo = os.path.expanduser('~/python-google-data/ldap-creds')
     with open(ldapInfo) as f:
       for line in f:
@@ -20,8 +22,10 @@ def connectLDAP():
           pWord = tail.strip()
         if head == 'server':
           ldapServer = tail.strip()
-        if head == 'basedn':
-          basedn = tail.strip()
+        if head == 'staffbasedn':
+          staffbasedn = tail.strip()
+        if head == 'memberbasedn':
+          memberbasedn = tail.strip()
     conn = Connection(ldapServer, uid, pWord, client_strategy=SAFE_SYNC, auto_bind=True)
     return conn
 
@@ -30,7 +34,17 @@ def connectLDAP():
 # ldapRecord = ldapSearch(mailalias)
 def ldapSearch(mailalias):
   conn = connectLDAP()
-  ldapRecord = conn.search(basedn, '(mailalias=' + mailalias + ')', attributes=['sn', 'givenName', 'manager', 'gaid', 'telephoneNumber'])
+  ldapRecord = conn.search(staffbasedn, '(mailalias=' + mailalias + ')', attributes=['sn', 'givenName', 'manager', 'gaid', 'telephoneNumber'])
+  return ldapRecord
+
+def ldapInfo(mailalias):
+  conn = connectLDAP()
+  ldapRecord = conn.search(staffbasedn, '(mailalias=' + mailalias + ')', attributes=['manager', 'title', 'department', 'division'])
+  return ldapRecord
+
+def ldapEmployeeType(email):
+  conn = connectLDAP()
+  ldapRecord = conn.search(staffbasedn, '(mail=' + email + ')', attributes=['employeeType'])
   return ldapRecord
 
 # Searches LDAP for specified fields using dn
@@ -39,4 +53,9 @@ def ldapSearch(mailalias):
 def ldapDN(dn):
   conn = connectLDAP()
   ldapRecord = conn.search(basedn, '(distinguishedname' + dn + ')', attributes=['sn', 'givenName', 'manager', 'gaid', 'telephoneNumber'])
+  return ldapRecord
+
+def ldapNumber(siebcustid):
+  conn = connectLDAP()
+  ldapRecord = conn.search(memberbasedn, '(siebcustid=' + siebcustid + ')', attributes=['sn', 'givenName', 'gaid', 'ReservedMailAliases', 'MailAliasCreateDate', 'MailAliasStatus', 'MailAliasUpdateDate'])
   return ldapRecord
